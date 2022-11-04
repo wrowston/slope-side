0142ac4655d416790441693b45735675edb3b379
Will Rowston <willrowston@gmail.com>
2022-11-04 16:19:21 -0400

    Initial Commit

---
+++ slope-side/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  slope-side
+//
+//  Created by Will Rowston on 11/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ slope-side/slope_sideApp.swift
@@ -0,0 +1,17 @@
+//
+//  slope_sideApp.swift
+//  slope-side
+//
+//  Created by Will Rowston on 11/4/22.
+//
+
+import SwiftUI
+
+@main
+struct slope_sideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
